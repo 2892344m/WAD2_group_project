df0aa46e5dad98b128c20c12cb96248ba2efe206
James Morrison <2892344m@student.gla.ac.uk>
2025-03-15 14:51:13 +0000

    Added the sidebar and pages for categories.

--- shop/views.py
+++ shop/views.py
@@ -11,8 +11,8 @@ from shop.forms import ProductForm, SearchForm
 #Also sends a list of all categories
 def homepage(request):
     context_dict = {}
-    context_dict['most_viewed_products'] = Product.objects.order_by('-views')
-    context_dict['recently_added_products'] = Product.objects.order_by('-date_added')
+    context_dict['most_viewed_products'] = Product.objects.order_by('-views')[:4]
+    context_dict['recently_added_products'] = Product.objects.order_by('-date_added')[:4]
     context_dict['category_list'] = Category.objects.order_by()
 
     return render(request, 'shop/homepage.html', context=context_dict)
@@ -34,8 +34,22 @@ def view_product(request, product_name_slug):
 
     return render(request, 'shop/view_product.html', context=context_dict)
 
-#Right now it just displays all products
-#Once search and categories are implemented, it will filter displayed products based off parameters
+#Displays all products present within a category
+def view_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        products = Product.objects.filter(category=category)
+        context_dict['products'] = products
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['products'] = None
+        context_dict['category'] = None
+
+    return render(request, 'shop/category.html', context=context_dict)
+
+#Displays producst based on search input
 def search(request):
     context_dict = {}
 
@@ -44,19 +58,22 @@ def search(request):
         if form.is_valid():
             #Takes in a search query from the user
             #Compares the set of characters in the search with the set of characters of the product, if less then appends to the filtered product list
-            #Finally, return the new product list to be displayed on the page (Not implemented yet)
+            #Finally, return the new product list to be displayed on the page
             search_query = form.cleaned_data['search']
             products = Product.objects.order_by()
             filtered_products = []
             for product in products:
                 if set(search_query.lower()) <= set(product.product_name.lower()):
-                    filtered_products.append(product.product_name)
-            return HttpResponse(str(filtered_products))
+                    filtered_products.append(product)
+
+            context_dict['form'] = form
+            context_dict['products'] = filtered_products
+
+            return render(request, 'shop/searchtml', context=context_dict)
     else:
         form = SearchForm()
 
     context_dict['form'] = form
-
     context_dict['products'] = Product.objects.order_by()
 
     return render(request, 'shop/search.html', context=context_dict)
