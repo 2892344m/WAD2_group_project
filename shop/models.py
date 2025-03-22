956c4363f4518390cfc4a7db04513edceb4704a6
James Morrison <2892344m@student.gla.ac.uk>
2025-03-22 16:03:19 +0000

    Unit tests for models, plus some bug fixes thanks to those unit tests.

--- shop/models.py
+++ shop/models.py
@@ -30,17 +30,22 @@ class Product(models.Model):
     seller = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     product_name = models.CharField(max_length=STRING_MAX_LENGTH)
-    price = models.FloatField(default=0)
-    quantity = models.IntegerField(default=0)
-    average_rating = models.FloatField(default=-1)
-    image_reference = models.ImageField(upload_to="product_img/")
-    views = models.IntegerField(default=0)
+    price = models.FloatField(default=0.01,validators=[MinValueValidator(0.01)])
+    quantity = models.IntegerField(default=1, validators=[MinValueValidator(1)])
+    average_rating = models.FloatField(default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    image_reference = models.ImageField(upload_to="product_img/", default='product_img/placeholder.jpg')
+    views = models.IntegerField(default=0, validators=[MinValueValidator(0)])
     date_added = models.DateField(default=date.today)
     description = models.CharField(max_length=DESCRIPTION_MAX_LENGTH)
     slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.product_name)
+        if self.views < 0: self.views = 0
+        if self.price < 0.01: self.price = 0.01
+        if self.quantity < 1: self.quantity = 1
+        if self.average_rating < 0: self.average_rating = 0
+        elif self.average_rating > 5: self.average_rating = 5
         super(Product, self).save(*args, **kwargs)
 
     def __str__(self):
@@ -50,7 +55,11 @@ class Product(models.Model):
 class Basket(models.Model):
     basket_owner = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, blank=True)
-    total_price = models.FloatField(default=0)
+    total_price = models.FloatField(default=0, validators=[MinValueValidator(0)])
+
+    def save(self, *args, **kwargs):
+        if self.total_price < 0: self.total_price = 0
+        super(Basket, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.basket_owner.username}'s basket"
@@ -59,7 +68,7 @@ class Basket(models.Model):
 class Order(models.Model):
     order_owner = models.ForeignKey(User, on_delete=models.CASCADE)
     products_to_deliver = models.ManyToManyField(Product, blank=True)
-    time_to_deliver = models.DateTimeField()
+    time_to_deliver = models.DateTimeField(default=date.today)
 
     def __str__(self):
         return f"{self.order_owner.username}'s order"
@@ -68,9 +77,13 @@ class Order(models.Model):
 class UserAccount(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     seller_account = models.BooleanField()
-    account_img = models.ImageField(upload_to='profile_img/')
-    balance = models.FloatField(default=0)
+    account_img = models.ImageField(upload_to='profile_img/', default='profile_img/default.svg')
+    balance = models.FloatField(default=0, validators=[MinValueValidator(0)])
     orders = models.ManyToManyField(Order, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.balance < 0: self.balance = 0
+        super(UserAccount, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.user.username
@@ -81,6 +94,11 @@ class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     rating = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(0)])
     comment = models.CharField(max_length=DESCRIPTION_MAX_LENGTH)
+
+    def save(self, *args, **kwargs):
+        if self.rating < 0: self.rating = 0
+        elif self.rating > 5: self.rating = 5
+        super(Review, self).save(*args,**kwargs)
 
     def __str__(self):
         return str(self.rating)
--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,259 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from shop.models import Basket, Category, Order, Product, Review, UserAccount, Wishlist
+
+# Model Tests
+
+#Category Tests
+#   Test that the category name works
+#   Test that a propery slug is created
+class CategoryModelTests(TestCase):
+    def test_ensure_unique_name(self):
+        category = add_category()
+        self.assertEqual((category.name == "test"), True)
+
+    def test_category_slug(self):
+        category = add_category(name="Unique and Interesting")
+        self.assertEqual((category.slug == "unique-and-interesting"), True)
+
+#User Account Tests
+    #Test that the placeholder image is properly added
+    #Test that an account can't have a negative balance
+class UserAccountModelTests(TestCase):
+    def test_create_account_no_img(self):
+        testUser = add_user()
+        account = add_user_account(testUser)
+
+        self.assertEqual((account.account_img == 'profile_img/default.svg'), True)
+
+    def test_account_positive_balance(self):
+        testUser = add_user()
+        account = add_user_account(user=testUser, balance=-1)
+
+        self.assertEqual((account.balance == 0), True)
+
+#Review Tests
+    #Test that a rating can't be less than 0
+    #Test that a rating can't be greater than 5
+class ReviewModelTests(TestCase):
+    def test_min_rating(self):
+        testUser = add_user()
+        testCategory = add_category()
+        testProduct = add_product(testUser, testCategory)
+        
+        review = add_review(reviewer=testUser,product=testProduct,rating=-3)
+
+        self.assertEquals((review.rating == 0), True)
+
+    def test_max_rating(self):
+        testUser = add_user()
+        testCategory = add_category()
+        testProduct = add_product(testUser, testCategory)
+        
+        review = add_review(reviewer=testUser,product=testProduct,rating=5)
+
+        self.assertEquals((review.rating == 5), True)
+
+#Product Tests
+    #Test that a price can't be less than a single penny
+    #Test that a product can't have less than one item in stock
+    #Test that a rating must be greater than zero
+    #Test that a rating must be less than five
+    #Test that a product must have at minimum zero views
+    #Test that a product uses the placeholder image if none is provided
+    #Test that product slug properly works
+class ProductModelTests(TestCase):
+    def test_min_price(self):
+        testUser = add_user()
+        testCategory = add_category()
+        testProduct = add_product(seller=testUser, category=testCategory, price=-3)
+        
+        self.assertEquals((testProduct.price == 0.01), True)
+
+    def test_min_quantity(self):
+        testUser = add_user()
+        testCategory = add_category()
+        testProduct = add_product(seller=testUser, category=testCategory, quantity=-3)
+        
+        self.assertEquals((testProduct.quantity == 1), True)
+
+    def test_min_rating(self):
+        testUser = add_user()
+        testCategory = add_category()
+        testProduct = add_product(seller=testUser, category=testCategory, average_rating=-3)
+        
+        self.assertEquals((testProduct.average_rating == 0), True)
+
+    def test_max_rating(self):
+        testUser = add_user()
+        testCategory = add_category()
+        testProduct = add_product(seller=testUser, category=testCategory, average_rating=8)
+        
+        self.assertEquals((testProduct.average_rating == 5), True)
+
+    def test_min_views(self):
+        testUser = add_user()
+        testCategory = add_category()
+        testProduct = add_product(seller=testUser, category=testCategory, views=-3)
+        
+        self.assertEquals((testProduct.views == 0), True)
+
+    def test_default_img(self):
+        testUser = add_user()
+        testCategory = add_category()
+        testProduct = add_product(seller=testUser, category=testCategory)
+        
+        self.assertEquals((testProduct.image_reference == 'product_img/placeholder.jpg'), True)
+
+    def test_product_slug(self):
+        testUser = add_user()
+        testCategory = add_category()
+        testProduct = add_product(seller=testUser, category=testCategory)
+        
+        self.assertEquals((testProduct.slug == "test-product-name"), True)
+
+#Wishlist Tests
+    #Test adding a product to the wishlist
+    #Test adding two products to the wishlist
+class WishlistModelTests(TestCase):
+    def test_add_product(self):
+        testUser = add_user()
+        testCategory = add_category()
+        testProduct = add_product(seller=testUser, category=testCategory)
+
+        wishlist=add_wishlist(wishlist_owner=testUser,products=[testProduct])
+
+        self.assertEquals((len(wishlist.products.all()) == 1),True)
+
+    def test_add_two_products(self):
+        testUser = add_user()
+        testCategory = add_category()
+        testProduct = add_product(seller=testUser, category=testCategory)
+        testProduct2 = add_product(seller=testUser, category=testCategory, product_name="test2")
+
+        wishlist=add_wishlist(wishlist_owner=testUser,products=[testProduct, testProduct2])
+
+        self.assertEquals((len(wishlist.products.all()) == 2),True)
+
+#Basket Tests
+    #Test adding a product to basket
+    #Test adding two products to a basekt
+class BasketModelTest(TestCase):
+    def test_add_product(self):
+        testUser = add_user()
+        testCategory = add_category()
+        testProduct = add_product(seller=testUser, category=testCategory)
+
+        basket=add_basket(basket_owner=testUser,products=[testProduct])
+
+        self.assertEquals((len(basket.products.all()) == 1), True)
+
+    def test_add_two_products(self):
+        testUser = add_user()
+        testCategory = add_category()
+        testProduct = add_product(seller=testUser, category=testCategory)
+        testProduct2 = add_product(seller=testUser, category=testCategory, product_name="test2")
+
+        basket=add_basket(basket_owner=testUser,products=[testProduct, testProduct2])
+
+        self.assertEquals((len(basket.products.all()) == 2), True)
+
+#Order Tests
+    #Test adding 1 product to order
+    #Test adding 2 products to order
+class OrderModelTests(TestCase):
+    def test_add_product(self):
+        testUser = add_user()
+        testCategory = add_category()
+        testProduct = add_product(seller=testUser, category=testCategory)
+
+        order=add_order(order_owner=testUser,products=[testProduct])
+
+        self.assertEquals((len(order.products_to_deliver.all()) == 1), True)
+
+    def test_add_two_products(self):
+        testUser = add_user()
+        testCategory = add_category()
+        testProduct = add_product(seller=testUser, category=testCategory)
+        testProduct2 = add_product(seller=testUser, category=testCategory, product_name="test2")
+
+        order=add_order(order_owner=testUser,products=[testProduct, testProduct2])
+
+        self.assertEquals((len(order.products_to_deliver.all()) == 2), True)
+
+#Views Tests
+class TestHomepage:
+    def test_homepage_with_products(self):
+        pass
+    
+
+
+# Helper Methods
+def add_user(username="testUsername", email="test@test.com", password="testPassword"):
+    user = User.objects.get_or_create(username=username, email=email, password=password)[0]
+    user.save()
+    return user
+
+def add_category(name="test"):
+    category = Category.objects.get_or_create(name=name)[0]
+    category.save()
+    return category
+
+def add_product(seller, category, product_name="Test Product Name", price=1,quantity=1,average_rating=0,views=0,description="test"):
+    product = Product.objects.get_or_create(seller=seller,
+                                        category=category,
+                                        product_name=product_name,
+                                        price=price,
+                                        quantity=quantity,
+                                        average_rating=average_rating,
+                                        views=views,
+                                        description=description)[0]
+    product.save()
+    return product
+
+def add_basket(basket_owner, products=[]):
+    basket = Basket.objects.get_or_create(basket_owner = basket_owner)[0]
+
+    for product in products:
+        basket.products.add(product)
+
+    basket.save()
+    return basket
+
+def add_wishlist(wishlist_owner, products=[]):
+    wishlist = Wishlist.objects.get_or_create(wishlist_owner=wishlist_owner)[0]
+
+    for product in products:
+        wishlist.products.add(product)
+
+    wishlist.save()
+
+    return wishlist
+
+def add_order(order_owner, products=[]):
+    order = Order.objects.get_or_create(order_owner=order_owner)[0]
+
+    for product in products:
+        order.products_to_deliver.add(product)
+
+    order.save()
+    
+    return order
+
+def add_user_account(user,seller_account=False,balance=0):
+    user_account = UserAccount.objects.get_or_create(user=user,
+                                                     seller_account=seller_account,
+                                                     balance=balance)[0]
+    user_account.save()
+
+    return user_account
+
+def add_review(reviewer,product,rating=0,comment="test"):
+    review = Review.objects.get_or_create(reviewer=reviewer,
+                                          product=product,
+                                          rating=rating,
+                                          comment=comment)[0]
+    review.save()
+
+    return review
