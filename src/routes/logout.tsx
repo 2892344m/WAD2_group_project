0d8ce82323de87d559ceb01fdd3804e15a56cd4b
Sam Lynch <158608123+2790363L@users.noreply.github.com>
2026-03-24 08:21:08 +0000

    Delete src directory

--- src/routes/logout.tsx
+++
@@ -1,7 +0,0 @@
-import { deleteCookie } from 'hono/cookie';
-import type { Context } from 'hono';
-
-export const onRequestGet = async (c: Context) => {
-  deleteCookie(c, 'session', { path: '/' });
-  return c.redirect('/login');
-};
