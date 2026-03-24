37d9c344690af5f80df033482d98f25ced561587
Sam Lynch <158608123+2790363L@users.noreply.github.com>
2026-03-24 08:22:28 +0000

    Delete drizzle.config.ts

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-// drizzle.config.ts
-import { defineConfig } from 'drizzle-kit';
-export default defineConfig({
-  dialect: 'sqlite',
-  schema: './src/models/schema.ts',
-  out: './drizzle',
-
-  dbCredentials: {url: './db.sqlite',},
-});
